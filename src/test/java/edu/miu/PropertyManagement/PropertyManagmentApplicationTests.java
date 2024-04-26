72669caaf5d69e74c9188a33d0d95c828f280e68
teke1 <teke156605@gmail.com>
2024-04-26 13:46:06 -0400

    CI

--- src/test/java/edu/miu/PropertyManagement/PropertyManagmentApplicationTests.java
+++ src/test/java/edu/miu/PropertyManagement/PropertyManagmentApplicationTests.java
@@ -3,7 +3,7 @@ package edu.miu.PropertyManagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class PropertyManagmentApplicationTests {
 
 	@Test
